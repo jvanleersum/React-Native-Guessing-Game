d0319e75536ad24089d337dfe185516c4d618901
Judith van Leersum <judithvleersum@gmail.com>
2022-08-10 16:50:46 +0200

    updating StartGameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,14 +1,34 @@
-import { View, TextInput, Button } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
-  return <View>
+  return (
     <View>
-      <TextInput />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.numberInput} />
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+      </View>
     </View>
-  </View>
-}
+  );
+};
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25
+  },
+  numberInput: {
+    
+  }
+});
