44636001efd793f4e0b7168909780de3a4775f6b
Judith van Leersum <judithvleersum@gmail.com>
2022-08-10 14:32:50 +0200

    Setup 3 screens

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import { View, TextInput, Button } from "react-native-web";
+
+const StartGameScreen = () => {
+  return <View>
+    <TextInput />
+    <View>
+      <Button />
+      <Button />
+    </View>
+  </View>
+}
+
+export default StartGameScreen;
